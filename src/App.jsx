b68b412446ed4169562c7a67b61f85e5ad4b5631
Baisali-akrabhi <baisali.pradhan@infoviz.co>
2024-04-18 20:19:17 +0530

    WIP: QuillAudit Project- css fixed and issue-description page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,14 @@ import DashBoard from "./pages/DashBoard/Dashboard";
 const LazyInnerIssue = React.lazy(() =>
   import("./components/issueInnerList/IssueInnerList")
 );
-const LazyInnerIssueDescription = React.lazy(() =>
-  import("./components/issueDescription/IssueDescription")
-);
-
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<DashBoard />} />
-        <Route
-          path="/issue"
-          element={
-            <React.Suspense fallback="Loading...">
-              <LazyInnerIssue />
-            </React.Suspense>
-          }
-        />
-         <Route
-          path="/issueDescription"
-          element={
-            <React.Suspense fallback="Loading...">
-              <LazyInnerIssueDescription />
-            </React.Suspense>
-          }
-        />
       </Routes>
     </BrowserRouter>
- 
   );
 };
 
