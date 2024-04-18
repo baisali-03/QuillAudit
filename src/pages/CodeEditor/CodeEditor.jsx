39f3646e29e1e5e05676772565705a870ce66dd9
Baisali-akrabhi <baisali.pradhan@infoviz.co>
2024-04-18 12:18:52 +0530

    WIP: QuillAudit Project- navbar link added to quillaudit netlify

--- src/pages/CodeEditor/CodeEditor.jsx
+++ src/pages/CodeEditor/CodeEditor.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import * as monaco from "monaco-editor";
+import IconButton from "@mui/material/IconButton";
+import ClearIcon from '@mui/icons-material/Clear';
 import "./CodeEditor.css";
 
-const CodeEditor = ({ code, language }) => {
+const CodeEditor = ({ code, language, onCancel }) => {
   React.useEffect(() => {
     const editor = monaco.editor.create(document.getElementById("editor"), {
       value: code,
@@ -18,11 +20,22 @@ const CodeEditor = ({ code, language }) => {
     };
   }, [code, language]);
 
+  const handleCancel = () => {
+    // Perform cancel action here
+    if (typeof onCancel === "function") {
+      onCancel();
+    }
+  };
+
   return (
     <div className="codeEditorContainer ">
       <div className="fileName">
-        <div className="fileName1">Sample File  <span style={{paddingLeft:20}}> X</span></div>
-
+        <div className="fileName1">
+          Sample File 
+          <IconButton onClick={handleCancel} className="cancelButton" style={{ color: 'white' }}>
+            <ClearIcon />
+          </IconButton>
+        </div>
       </div>
       <div id="editor" className="codeEditor" />
     </div>
