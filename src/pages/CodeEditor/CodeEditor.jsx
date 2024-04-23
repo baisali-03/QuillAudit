bbd1de5664b5c64a18f0344eb4999abbdad53156
Baisali-akrabhi <baisali.pradhan@infoviz.co>
2024-04-23 15:22:09 +0530

    WIP: QuillAudit Project- css fixed

--- src/pages/CodeEditor/CodeEditor.jsx
+++ src/pages/CodeEditor/CodeEditor.jsx
@@ -12,7 +12,7 @@ const CodeEditor = ({
   onHideFileFolder,
   onHideIssueSection,
   showFileFolder,
-  showIssueSection
+  showIssueSection,
 }) => {
   React.useEffect(() => {
     // Define custom theme for code-editor
@@ -35,7 +35,7 @@ const CodeEditor = ({
       lineNumbers: "on",
       minimap: { enabled: false },
     });
-    
+
     return () => {
       editor.dispose();
     };
@@ -50,38 +50,59 @@ const CodeEditor = ({
   };
 
   //-------------Condition to increase the width of code editor-------------------
-  const editorWidth = showFileFolder && showIssueSection ? "42vw" : showFileFolder || showIssueSection ? "68vw" : "96vw";
+  const editorWidth =
+    showFileFolder && showIssueSection
+      ? "42vw"
+      : showFileFolder || showIssueSection
+      ? "68vw"
+      : "96vw";
 
   //-----------------Condition to rotate arrow icon for show folder---------------------
   const LeftarrowRotation = showFileFolder ? 0 : 180;
 
- //----------------Condition to rotate arrow icon for show Issue Section-----------------
- const RightarrowRotation = showIssueSection ? 0 : 180;
+  //----------------Condition to rotate arrow icon for show Issue Section-----------------
+  const RightarrowRotation = showIssueSection ? 0 : 180;
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <div className="relative bg-darkBorder ml-3">
-        <div className="absolute top-0 left-0 right-0 w-full h-10 p-0 flex justify-between text-sm rounded items-center">
+        <div className="absolute top-0 left-0 right-0 w-full h-10 p-0 flex justify-start text-sm rounded items-center">
           <div className="text-white bg-blue-600 rounded-t-sm h-10 flex justify-center items-center w-32">
-            Sample File
+            SampleFile-1
             <IconButton className="cancelButton" style={{ color: "white" }}>
               <ClearIcon />
             </IconButton>
           </div>
-        </div>
-        <div className="text-white bg-darkBlue h-10 flex rounded justify-center items-center w-full">
-          Sample Project
-          <IconButton className="cancelButton" style={{ color: "white" }}>
-            <ClearIcon />
-          </IconButton>
-          <div className="h-10 mr-2 border border-solid border-darkBorder"></div>
-          Sample Project
-          <IconButton className="cancelButton" style={{ color: "white" }}>
-            <ClearIcon />
-          </IconButton>
+          <div className="text-white rounded-t-sm h-10 pl-2 flex justify-center items-center w-36">
+            SampleFile-2
+            <IconButton className="cancelButton" style={{ color: "white" }}>
+              <ClearIcon />
+            </IconButton>
+          </div>
+
+          <div className="h-10 mr-1 border border-solid border-darkBorder"></div>
+          <div className="text-white rounded-t-sm h-10 pl-2 flex justify-center items-center w-36">
+            SampleFile-3
+            <IconButton className="cancelButton" style={{ color: "white" }}>
+              <ClearIcon />
+            </IconButton>
+          </div>
+          <div className="h-10 mr-1 border border-solid border-darkBorder"></div>
+          <div className="text-white  rounded-t-sm h-10 pl-2 flex justify-center items-center w-36">
+            SampleFile-4
+            <IconButton className="cancelButton" style={{ color: "white" }}>
+              <ClearIcon />
+            </IconButton>
+          </div>
+          
           <div className="h-10 mr-1 border border-solid border-darkBorder"></div>
         </div>
-        <div id="editor" className="codeEditor" style={{ width: editorWidth }} />
+        <div className="text-white bg-darkBlue h-10 flex rounded justify-center items-center w-full"></div>
+        <div
+          id="editor"
+          className="codeEditor"
+          style={{ width: editorWidth }}
+        />
       </div>
       <div className="absolute bottom-0 left-0">
         <IconButton
@@ -91,10 +112,10 @@ const CodeEditor = ({
             color: "white",
             backgroundColor: "#007AFF",
             borderRadius: "2px",
-            padding:'4px',
-            height:20,
-            width:20,
-            marginBottom:'20px',
+            padding: "4px",
+            height: 20,
+            width: 20,
+            marginBottom: "20px",
             transform: `rotate(${LeftarrowRotation}deg)`,
           }}>
           <ArrowBackIcon />
@@ -108,11 +129,11 @@ const CodeEditor = ({
             color: "white",
             backgroundColor: "#007AFF",
             borderRadius: "2px",
-            padding:'4px',
-            height:20,
-            width:20,
-            marginBottom:'20px',
-            transform: `rotate(${RightarrowRotation}deg)`, 
+            padding: "4px",
+            height: 20,
+            width: 20,
+            marginBottom: "20px",
+            transform: `rotate(${RightarrowRotation}deg)`,
           }}>
           <ArrowForwardIcon />
         </IconButton>
